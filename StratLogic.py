f9c7424636db06ece9705123d6731eb8c8907248
Najjad <80614053+Najjad@users.noreply.github.com>
2022-06-09 22:40:55 +0400

    Update StratLogic.py

--- StratLogic.py
+++ StratLogic.py
@@ -2,9 +2,13 @@ from difficulty import difficulty
 from averaging import avg
 import os
 import subprocess
+from subprocess import Popen
 from time import time, sleep
 
-def process_exists(process_name):
+here = os.getcwd()
+start_rvn = os.path.join(here, 'start_rvn.bat')
+#pyinstaller is the most stressful thing I have ever used, can't find any files unless directory specified
+def process_exists(process_name): #function I stole to find out if a process is running or not
   call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
   #use buildin check_output right away
   output = subprocess.check_output(call).decode()
@@ -16,18 +20,20 @@ def process_exists(process_name):
 
 def StratMine():
  while True:
-    sleep(5 - time() % 5)
-    if difficulty() > avg():
-      if process_exists("nbminer.exe") is True:
-       os.system("TASKKILL /F /IM nbminer.exe")
+    sleep(900 - time() % 900) #change time in seconds
+    if difficulty() > avg(): # if difficulty is greater than the average
+      if process_exists("nbminer.exe") is True: #check if miner is currently running
+       os.system("TASKKILL /F /IM nbminer.exe") #kill process if mining is running
+       print("current difficulty currently higher than average difficulty")
       else:
-        print("difficulty currently higher than average")
-    elif difficulty() < avg(): #if difficulty is less than average, mine
+        print("current difficulty currently higher than average difficulty") #telling client that difficulty is too high
+
+    if difficulty() < avg(): #if difficulty is less than average, mine
       if process_exists("nbminer.exe") is True: #if nbminer is already running
-       os.system("TASKKILL /F /IM nbminer.exe") #exit nbminer
-       subprocess.Popen(r'DIRECTORY_OF_START_RVN', creationflags=subprocess.CREATE_NEW_CONSOLE)#rerun nbminer
+       print("NBminer already running")
 
       elif  process_exists("nbminer.exe") is False: #if nbminer is not running
-        subprocess.Popen(r'DIRECTORY_OF_START_RVN', creationflags=subprocess.CREATE_NEW_CONSOLE)#run nbminer
-
+        subprocess.Popen(start_rvn, creationflags=subprocess.CREATE_NEW_CONSOLE) 
+        #create new console = open seperate console window, not to run in terminal
+        
 
